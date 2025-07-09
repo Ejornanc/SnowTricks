e4b37a6b82966099f8ad867bace753e773cca952
DnD-Jorel <alexandre.delcroix@dnd.fr>
2025-07-09 05:18:27 +0200

    feat: finalize password reset implementation
    - Fixed missing base.html.twig layout for template inheritance
    - Corrected ChangePasswordFormType namespace import in ResetPasswordController
    - Created reset_password/reset_email.html.twig template for reset email content
    - Tested reset password route rendering and token verification

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Nouveau mot de passe',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+    }
+}
